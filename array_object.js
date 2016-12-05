20f0bdd669b3e2d6540d9744ce4954936ceaa471
Yoscroll <ianhenry.c808@gmail.com>
2016-12-05 08:39:19 -1000

    12/5/16

--- array_object.js
+++ array_object.js
@@ -1,7 +1,21 @@
 /*
-
   Ian Acosta
   Arrays and Objects
   Dec. 5, 2016
-  
 */
+
+//arrays
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior TAN","Zakattak","Yolo Baggins","Ohai","Basic Jeff"];
+var colors = ["light black","violet","purple"];
+var adj = ["smart","special case","yummy","tiny whiny","ferocious"];
+var slang = ["scrap","kobe beef","da kine","li 'dat","pau hana"];
+
+
+//objects
+var donut ={
+  toppings: ["sprinkles","chocolate frosting","air"],
+  fillings: "jelly",
+  shape: "square",
+  count: 12
+};
